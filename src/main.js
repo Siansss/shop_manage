2a247da62ba917ed5c06a804d1134809671a1715
小猪佩奇 <1666094489@qq.com>
2021-10-31 23:20:34 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,11 +8,19 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 import axios from 'axios'
 Vue.prototype.$http = axios
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 通过请求拦截器添加token，请求服务器前进行预处理，保证拥有获取数据的权限
+// 每次通过axios请求，都会追加一个token参数
 axios.interceptors.request.use(config => {
   // console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -22,6 +30,24 @@ axios.interceptors.request.use(config => {
 Vue.config.productionTip = false
 
 Vue.component('tree-table',TreeTable)
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 时间格式化的过滤器
+Vue.filter('dataFormat',function(originVal){
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2,'0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
